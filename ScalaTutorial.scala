8e2649317abc0ca4d5fe169ea5169fd4aa69dc3f
SincerelyBrittany <sincerelybr@gmail.com>
2021-01-12 14:19:44 -0500

    walking through with youtube video

--- ScalaTutorial.scala
+++ ScalaTutorial.scala
@@ -1,11 +1,81 @@
+//to compile: scalac ScalaTutorial.scala
+// or scala ScalaTutorial.scala
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//         while(i <= 10){
+//             println(i)
+//             i += 1
+//         }
+//     }
+// }
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//         do {
+//             println(i)
+//             i += 1
+//         } while ( i <= 20)
+//     }
+// }
+
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//         for (i <- 1 to 10)
+//         println(i)
+//     }
+// }
+
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//         val randLetters = "KJDHFSIUHFBFDUYSIUFHWOEIFH"
+//         for(i <-0 until randLetters.length)
+//             println(randLetters(i))
+
+//     }
+// }
+
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//         val aList = List(1,2,3,4,5)
+//         for(i <- aList){
+//         println("List Item " + i)
+//         }      
+//     }
+// }
+
+
+// object ScalaTutorial{
+//     def main(args: Array[String]){
+//         var i = 0
+//           var evenList = for { i <- 1 to 20
+//             // You can put as many conditons here separated with semicolons
+//             // as you need
+//             if (i % 2) == 0
+//             } yield i
+
+//     println("Even List")
+//     for(i <- evenList)
+//       println(i)  
+//     }
+// }
+
 object ScalaTutorial{
     def main(args: Array[String]){
         var i = 0
-        while(i <= 10){
-            println(i)
-            i += 1
-        }
+           for (i <- 1 to 5; j <- 6 to 10){
+                println("i: " + i)
+                println("j: " + j)
+         }
     }
 }
-//to compile: scalac ScalaTutorial.scala
-// or scala ScalaTutorial.scala
+
+
